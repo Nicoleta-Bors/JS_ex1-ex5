b52186598b5387838bcbfc814979ea4b7d99d65a
Bors Nicoleta <nicutabors@gmail.com>
2021-06-21 14:33:14 +0300

    Ex 2 added

---
+++ Ex2_Dog_Years/app.js
@@ -0,0 +1,12 @@
+const myAge = 19; //My age
+let earlyYears = 2; //this value will change
+earlyYears = earlyYears * 10.5;
+
+let laterYears = myAge - 2; //first 2 years
+
+laterYears *= 4 //the number of dog years accounted for by your later years
+
+const myAgeInDogYears = earlyYears + laterYears; //My age in dog years
+
+myName = 'Nicoleta'.toLowerCase(); //The toLowerCase method returns my name with all lowercase letters
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`); //display my name and age in dog years
